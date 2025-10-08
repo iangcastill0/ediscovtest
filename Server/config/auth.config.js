3c95c873d54b88294d44da750f5538e35baa0be7
ian castillo <iancastillo790@yahoo.com>
2025-10-08 14:28:09 -0400

    .env

--- Server/config/auth.config.js
+++ Server/config/auth.config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 module.exports = {
-  secret: '!@#_evestigate_saas_2023_!@#',
-  G_CLIENT_ID: '22301354270-s9o0tjfhhik2orirp2075bk63e7amf9e.apps.googleusercontent.com',
-  G_CLIENT_SECRET: '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',
+  secret: process.env.EXPRESS_SESSION_SECRET,
+  G_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  G_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   Twilio: {
     accountSID: process.env.TWILIO_ACCOUNT_SID,
     accountAuthToken: process.env.TWILIO_ACCOUNT_AUTH_TOKEN,
